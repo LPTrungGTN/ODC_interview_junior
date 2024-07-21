a7ae6e024bcd9db004db12640d974ad9c73d8b1f
LePhuongTrung <trunglp2001@gmail.com>
2024-07-21 18:46:37 +0700

    remove errorHandler

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,25 +4,20 @@ import helmet from 'helmet';
 import { pino } from 'pino';
 
 import taskRoutes from './api/routes/taskRoutes';
-import { errorHandler } from './common/middleware/errorHandler';
 
 const logger = pino({ name: 'server' });
 const app: Express = express();
 
 app.use(cors());
 app.use(helmet());
+app.use(express.json());
 
 app.use((req, res, next) => {
   logger.info(`${req.method} ${req.path}`);
   next();
 });
 
-app.use(express.json());
-
 app.use('/api/tasks', taskRoutes);
-app.get('', (req, res) => {
-  res.send('Hello World');
-});
-app.use(errorHandler);
+app.get('', (req, res) => res.send('Hello World'));
 
 export default app;
--- backend/src/common/middleware/errorHandler.ts
+++
@@ -1,9 +0,0 @@
-import { ErrorRequestHandler } from 'express';
-
-export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
-  if (res.headersSent) {
-    return next(err);
-  }
-  console.error(err);
-  res.status(500).json({ error: 'Internal Server Error' });
-};
